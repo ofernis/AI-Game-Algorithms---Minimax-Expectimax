25ad16fa2e4417e950e31ef516577c50893e73c1
roeik <roeikrinitz15@gmail.com>
2022-12-15 15:55:17 +0200

    maple story name

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
     # PART1
     game.play_game("human", "human")
-
+    xXOferKingXx
     # PART2
     # game.play_tournament("greedy", "random", 50)
     # game.play_tournament("greedy_improved", "random", 50)
